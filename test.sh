c4fc24e30340fe0db7016e59e844cdeb1159ff47
Ron Zuckerman <ron.zuckerman@adtran.com>
2023-12-10 15:53:09 -0600

    Fix path to compiled output

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ DOCKER_RUN="docker run --rm -i -v $(pwd):/local -w /local ${DOCKER_IMAGE}"
 
 CMD="ec -stop hello_world.e 1>&2 && \
     ./EIFGENs/hello_world/W_code/hello_world && \
-    rm -rf hello_world hello_world.efc EIFGENs/"
+    rm -rf hello_world hello_world.ecf EIFGENs/"
 RESULT="$(${DOCKER_RUN} sh -c "${CMD}")"
 echo "${RESULT}"
 if [ "${RESULT}" = "Hello, world!" ]
